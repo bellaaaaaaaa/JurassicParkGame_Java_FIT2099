d4f2366d81a959123d282eec4dfb657c07afcdf8
Gabriela Bayabos <gbay0002@student.monash.edu>
2020-10-18 19:59:28 +0800

    Added documentation for Food class

--- src/edu/monash/fit2099/engine/Food.java
+++ src/edu/monash/fit2099/engine/Food.java
@@ -1,11 +1,23 @@
 package edu.monash.fit2099.engine;
 
-public class Food extends Item {
+/**
+ * Food for dinosaurs
+ */
+public abstract class Food extends Item {
     int foodLvl;
     int price;
     int gainPoints;
     String name;
 
+    /**
+     * This method is for a type of food based on the Item constructor.
+     * @param foodLvl The amount of points that when eaten, increases a dinosaurs foodLvl by
+     * @param price The number of eco points required to purchase this item from a vending machine
+     * @param gainPoints The number of eco points a player gains each time they feed a specific food to a dinosaur
+     * @param name The name used to identify the type of food. Eg. Fruit.
+     * @param displayChar The character used to display the food
+     * @param portable A boolean indicating whether an instance can be stored in the players inventory/ moved around
+     */
     public Food(int foodLvl, int price, int gainPoints, String name, char displayChar, boolean portable){
         super(name, displayChar, portable);
         this.foodLvl = foodLvl;
